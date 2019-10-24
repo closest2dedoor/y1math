2d92992673cc0e8e1594446f3740f7396763281e
closest2dedoor <52991745+closest2dedoor@users.noreply.github.com>
2019-10-24 14:42:12 +0800

    Update t01primeshcflcm_primekth.py

--- t01primeshcflcm_primekth.py
+++ t01primeshcflcm_primekth.py
@@ -1,2 +1,21 @@
 # What is the kth prime number?
 
+def is_prime(x):
+    if x == 2:
+        return True
+    else:
+        for number in range (3,x): 
+            if x % number == 0 or x % 2 == 0:
+         #print number
+                return False
+        
+        return True
+
+lst = [2]
+for i in range(3,k):
+    if is_prime(i) == True:
+        lst.append(i)
+        #print lst
+        if len(lst) == 100:
+            print lst[9]
+            break
